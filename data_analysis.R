23d48066150305073d2c2920e146feccdeb28eda
Chesta Dewangan <cdewa3@uic.edu>
2025-04-23 16:02:41 -0500

    Add hypothesis R code

--- data_analysis.R
+++ data_analysis.R
@@ -1,6 +1,8 @@
 library(tidyverse)
+library(dplyr)
 
-df<-read.csv("total_data.csv")
+# Loading csv
+df<-read.csv("Final_Data_R.csv")
 
 # HH:MM:SS to seconds function
 time_to_seconds <- function(time_str) {
@@ -15,18 +17,73 @@ time_to_seconds <- function(time_str) {
 df$NoteTakingTime_sec <- sapply(df$NoteTakingTime, time_to_seconds)
 df$RetrievalTime_sec <- sapply(df$RetrievalTime, time_to_seconds)
 
-df$Participant
+# 1 - Digital and 2 - Physical
+df$Preference <- factor(df$Preference, levels = c("1", "2"))
+df$Preference
 
-df$Preference <- factor(df$Preference, levels = c("Digital", "Physical"))
+"-----------------------------------------------------------------------------------------------"
+"Hypothesis 1 - Physical note-taking participants will take significantly more time 
+for both note-taking and retrieval tasks than digital note-taking participants.
+Since both the time were not normal we can use."
 
+# First we will do normality tests on Note-Taking Time and Retrieval Time
 # Note-Taking Time
 shapiro.test(df$NoteTakingTime_sec)
+# NoteTakingTime_sec - Resulted in not normal 
 
 # Retrieval Time
 shapiro.test(df$RetrievalTime_sec)
+# RetrievalTime_sec - Resulted in not normal 
 
-# Hypothesis 1
+# Using non-parametric tests
+# Note-Taking Time: Physical vs Digital
+wilcox.test(NoteTakingTime_sec ~ Preference, data = df)
+
+# Retrieval Time: Physical vs Digital
+wilcox.test(RetrievalTime_sec ~ Preference, data = df)
+
+# The test revealed no significant differences in:
+# Note-taking time (W = 2, p = 0.53)
+# Retrieval time (W = 5, p = 0.80)
+
+# Since inferential tests doesn't give much insights, we will now use descriptive stats
+
+df %>%
+  group_by(Preference) %>%
+  summarise(
+    Mean_Note = mean(NoteTakingTime_sec, na.rm = TRUE),
+    Median_Note = median(NoteTakingTime_sec, na.rm = TRUE),
+    SD_Note = sd(NoteTakingTime_sec, na.rm = TRUE),
+    Mean_Retrieval = mean(RetrievalTime_sec, na.rm = TRUE),
+    Median_Retrieval = median(RetrievalTime_sec, na.rm = TRUE),
+    SD_Retrieval = sd(RetrievalTime_sec, na.rm = TRUE),
+    N = n()
+  )
+
+"On average, digital note-takers (n = 4) spent 1,892 seconds on note-taking (SD = 2,384), while 
+physical note-takers (n = 2) spent 1,960 seconds (SD = 713). Despite a slightly higher mean for 
+the physical group, the digital group had a much lower median (777 sec), indicating high 
+variability — likely driven by one or more long note-taking sessions."
+
+boxplot(NoteTakingTime_sec ~ Preference, data = df,
+        main = "Note-Taking Time by Method", ylab = "Seconds")
+
+boxplot(RetrievalTime_sec ~ Preference, data = df,
+        main = "Retrieval Time by Method", ylab = "Seconds")
+
+# The above results can be also be verified by looking at the box plots and see the high variability.
+
+
+"-----------------------------------------------------------------------------------------------"
+
 
 # Hypothesis 2
 
+
+"-----------------------------------------------------------------------------------------------"
+
+
 # Hypothesis 3
+
+
+"-----------------------------------------------------------------------------------------------"
