0b72bbda28d384aa570e7801bd6edda6eeb09654
Mishri Patel <mishri7902@gmail.com>
2025-04-25 09:50:53 -0500

    Added H2

--- data_analysis.R
+++ data_analysis.R
@@ -77,7 +77,44 @@ boxplot(RetrievalTime_sec ~ Preference, data = df,
 "-----------------------------------------------------------------------------------------------"
 
 
-# Hypothesis 2
+# Hypothesis 2 - "Physical note-taking participants will report lower self-perceived retrieval efficiency"
+
+data <- data %>%
+  mutate(
+    RetrievalScore = rowMeans(data[, c("Retrieval1", "Retrieval2", "Retrieval3", "Retrieval4", "Retrieval5", "Retrieval6")], na.rm = TRUE),
+    Preference = factor(Preference, levels = c(1, 2), labels = c("Physical", "Digital"))
+  )
+
+
+#Filtering missing values
+filtered_data <- data %>%
+  filter(!is.na(Preference) & !is.na(RetrievalScore))
+
+
+#Normality checking
+shapiro.test(filtered_data$RetrievalScore)
+
+# Wilcoxon tests
+cat("\n== Retrieval Score Test (Physical < Digital) ==\n")
+print(wilcox.test(RetrievalScore ~ Preference,
+                  data = filtered_data,
+                  alternative = "less",
+                  exact = FALSE))
+
+# Descriptive stats
+filtered_data %>%
+  group_by(Preference) %>%
+  summarise(
+    Count = n(),
+    Mean_RetrievalScore = mean(RetrievalScore),
+    Median_RetrievalScore = median(RetrievalScore),
+    SD_RetrievalScore = sd(RetrievalScore)
+  )
+
+
+boxplot(RetrievalScore ~ Preference, data = filtered_data,
+        main = "Self-Perceived Retrieval Efficiency",
+        col = c("skyblue", "lightgreen"))
 
 
 "-----------------------------------------------------------------------------------------------"
