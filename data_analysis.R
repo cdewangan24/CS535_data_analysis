7d7d530d50a8bf89b5bbc4659386a1ddfbac9544
Huma Hussain <hhussain0901@gmail.com>
2025-04-25 09:19:46 -0500

    added h3

--- data_analysis.R
+++ data_analysis.R
@@ -83,7 +83,41 @@ boxplot(RetrievalTime_sec ~ Preference, data = df,
 "-----------------------------------------------------------------------------------------------"
 
 
-# Hypothesis 3
+# Hypothesis 3 - Physical note-taking participants will report higher levels of concern related to sustainability.
+
+# Convert Preference to labeled factor
+df$Preference <- factor(df$Preference, levels = c("1", "2"), labels = c("Digital", "Physical"))
+
+# Filter out missing Sustainability data
+sustain_data <- df %>%
+  filter(!is.na(Sustainability1))
+
+#Normality test
+shapiro.test(sustain_data$Sustainability1)
+
+# Non-parametric test
+wilcox.test(Sustainability1 ~ Preference, data = sustain_data)
+
+#Descriptive Stats
+sustain_data %>%
+  group_by(Preference) %>%
+  summarise(
+    Mean = mean(Sustainability1, na.rm = TRUE),
+    Median = median(Sustainability1, na.rm = TRUE),
+    SD = sd(Sustainability1, na.rm = TRUE),
+    N = n()
+  )
+
+#Box Plot
+boxplot(Sustainability1 ~ Preference, data = sustain_data,
+        main = "Sustainability Concern by Note-Taking Method",
+        ylab = "Sustainability Concern",
+        col = c("skyblue", "lightgreen"))
+
+
+
+
+
 
 
 "-----------------------------------------------------------------------------------------------"
